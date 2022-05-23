8ddfed211a76d213060dcb9b77c66648a9f13f9b
Hugo Rios <hugostaq@gmail.com>
2022-05-23 00:55:46 -0300

    Alert Added

--- third-app.js
+++ third-app.js
@@ -1,4 +1,5 @@
 let htmlText = document.getElementById("text")
 document.getElementById("changeTextBtn").onclick = function changeText() {
     htmlText.style.color = "green"
+    alert("The Color of The Text Is Now Green!")
 }
